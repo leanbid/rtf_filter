6761fdc1b66496c9e1bca13d11cf8b621ea3bcaf
gerald <gerald@gerald-desktop>
2011-12-08 17:17:41 +0000

    tweak regex

--- lib/rtf_filter.rb
+++ lib/rtf_filter.rb
@@ -26,7 +26,8 @@ module RtfFilter
     begin
       destination_folder += "/"unless destination_folder.empty?
       destination = destination_folder + filename.sub(/.{3}$/,'txt')
-    	system("rtffilter --source '"+filename+"' --destination '"+destination+"'"+self.allow_tags(allow_tags))
+      txt = self.to_txt(filename, options)
+      File.open(destination, 'w') {|f| f.write(txt)}
     rescue IOError => e 
       if options[:io_exception]
         raise e
@@ -45,8 +46,10 @@ module RtfFilter
   end
 
   def self.format_txt(txt)
-    txt.squeeze!("\n") #replace all occurrences of 2 or more line breaks with just one
-    txt.gsub!(/^\s+/, "") #delete leading whitespace (spaces/tabs/etc) from beginning of each line
+    txt.strip! #remove blank lines at begining and end
+    txt.gsub!(/^(|\n)[ \t\r]+/, "#{$1}") #delete leading whitespace (spaces/tabs) from beginning of each line
+    txt.gsub!(/\n{3,}/, '\n\n') #replace all occurrences of 3 or more line breaks with just two
+    return txt
   end
 
 end
