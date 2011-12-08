fd9a71a606931db8b523075e198209d6fcd2af54
gerald <gerald@gerald-desktop>
2011-12-08 18:00:39 +0000

    test to_txt method

--- test/test_rtf_filter.rb
+++ test/test_rtf_filter.rb
@@ -3,6 +3,14 @@ require 'rtf_filter'
 
 class RtfFilterTest < Test::Unit::TestCase
 
+  def test_should_convert_rtf_to_txt
+	assert_equal RtfFilter.to_txt('test/test_rtf_file.rtf'), "\n\n\n\n\n\n\n\n\n\ntest line 1\n \n test line 2\n \n test line 3\n "
+  end
+
+  def test_should_convert_rtf_to_txt_and_format_text
+	assert_equal RtfFilter.to_txt('test/test_rtf_file.rtf', :format_txt => true), "test line 1\n\ntest line 2\n\ntest line 3"
+  end
+
   def test_format_txt_should_take_out_extra_blank_lines_in_text
 	text = "l1\n\n\nl2\n\n\n\n\nL3\n\n\n\n\n\n\n\n\nL5\n\n\nL6"
 	assert_equal RtfFilter.format_txt(text), "l1\\n\\nl2\\n\\nL3\\n\\nL5\\n\\nL6" 
