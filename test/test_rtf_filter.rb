f407154ec6bd5415dfa097898b2b92fc7c12d3ca
gerald <gerald@gerald-desktop>
2012-01-27 14:22:22 +0000

    correct test

--- test/test_rtf_filter.rb
+++ test/test_rtf_filter.rb
@@ -19,9 +19,9 @@ class RtfFilterTest < Test::Unit::TestCase
 
   def test_format_txt_should_take_out_extra_blank_lines_in_text
 	text = "l1\n\n\nl2\n\n\n\n\nL3\n\n\n\n\n\n\n\n\nL5\n\n\nL6"
-	assert_equal RtfFilter.format_txt(text), "l1\\n\\nl2\\n\\nL3\\n\\nL5\\n\\nL6" 
+	assert_equal RtfFilter.format_txt(text), "l1\n\nl2\n\nL3\n\nL5\n\nL6" 
 	text = "l1\n\n\nl2\n\n  \n\n  \nL3\n\n\n\n \n\n\n\n\nL5\n\n\nL6"
-	assert_equal RtfFilter.format_txt(text), "l1\\n\\nl2\\n\\nL3\\n\\nL5\\n\\nL6" 
+	assert_equal RtfFilter.format_txt(text), "l1\n\nl2\n\nL3\n\nL5\n\nL6" 
   end
 
   def test_format_txt_should_take_out_blank_lines_at_begining_and_end_of_text
